9debfdd77e1fde1a96f2c3fb697e178e2dbce0e3
Fahim <hafijurrahman046@gmail.com>
2020-11-15 13:13:40 +0600

    effing commit

---
+++ precache-manifest.66d13997175a53470f258a34ea5b3938.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90be09bb58fde8d0bcb041b5eb06ed8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9088e809b56c3fb4eba7",
+    "url": "/static/css/2.d28c0021.chunk.css"
+  },
+  {
+    "revision": "c352bd2fe98536fcfbff",
+    "url": "/static/css/main.c9cb84ac.chunk.css"
+  },
+  {
+    "revision": "9088e809b56c3fb4eba7",
+    "url": "/static/js/2.28f58fbb.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.28f58fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c352bd2fe98536fcfbff",
+    "url": "/static/js/main.84c3b4a3.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
